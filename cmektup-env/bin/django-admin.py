9892767a4de83a82d912bf9050e41960388b1722
zertr <zeynela.ertur@gmail.com>
2018-11-25 00:30:28 +0300

    Django Projesi sample olarak oluşturuldu.

---
+++ cmektup-env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/zeynelabidin/Jobs/ceza_evine_mektup/website/cmektup-django/cmektup-env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
